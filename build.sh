9eacb595acf13f48df5a2009fb34c96b412082f9
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-05-21 18:22:52 +0300

    Remove redundant commadn

--- build.sh
+++ build.sh
@@ -8,11 +8,3 @@ cd ../test
 gerap doc std
 cd ../docs
 cp -r ../test/.gerap/docs ./build/docs
-
-BUILDER_LIB=$(find 'compiler/libs' -type f | tr '\n' ':')
-
-rm -rf gerac-gh
-git clone https://github.com/geralang/gerac gerac-gh
-cd gerac-gh
-make
-cd ..
