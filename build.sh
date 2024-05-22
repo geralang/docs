b859208b786568ce79825977966b9f7bf989cb79
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-05-22 18:55:15 +0300

    Switch to bytecoder

--- build.sh
+++ build.sh
@@ -1,46 +1,64 @@
 #!/bin/bash -e
 
-rm -rf build
+rm -rf "build"
 
-cp -r ./src ./build
+cp -r "./src" "./build"
 
-cd ../test
+mkdir -p "build-cache"
+
+# Generate stdlib documentation
+if [ ! -d "build-cache/gerap_doc_proj" ]; then
+    cd "build-cache"
+    gerap new "gerap_doc_proj"
+    cd ..
+fi
+cd "build-cache/gerap_doc_proj"
 gerap doc std
-cd ../docs
-cp -r ../test/.gerap/docs ./build/docs
+cd ../..
+cp -r "build-cache/gerap_doc_proj/.gerap/docs" "./build/docs"
 
-BUILDER_LIB=$(find 'compiler/libs' -type f | tr '\n' ':')
+# Get Bytecoder if not present
+if [ ! -f "build-cache/bytecoder.jar" ]; then
+    curl "https://repo.maven.apache.org/maven2/de/mirkosertic/bytecoder/bytecoder-cli/2024-05-10/bytecoder-cli-2024-05-10-executable.jar" \
+        -o "build-cache/bytecoder.jar"
+fi
+if [ ! -f "build-cache/bytecoder-api.jar" ]; then
+    curl "https://repo.maven.apache.org/maven2/de/mirkosertic/bytecoder/bytecoder.api/2024-05-10/bytecoder.api-2024-05-10.jar" \
+        -o "build-cache/bytecoder-api.jar"
+fi
 
-#rm -rf gerac-gh
-#git clone https://github.com/geralang/gerac gerac-gh
-#cd gerac-gh
-#make
-#cd ..
-mkdir gerac-gh -p
-cp gerac.jar gerac-gh/gerac.jar
+# Compile gerac if not present
+if [ ! -f "build-cache/gerac.jar" ]; then
+    rm -rf build-cache/gerac-gh
+    git clone "https://github.com/geralang/gerac" "build-cache/gerac-gh"
+    cd "build-cache/gerac-gh"
+    make
+    cd ../..
+    cp "build-cache/gerac-gh/gerac.jar" "build-cache/gerac.jar"
+fi
 
-mkdir compiler/out -p
-javac \
-    -cp "$BUILDER_LIB" \
-    $(find compiler/builder -name "*.java") \
-    -d compiler/out/builder
-cd "compiler/out/builder"
-jar -cef typesafeschwalbe.Builder ../builder.jar *
-cd ../../..
-javac \
-    -cp "$BUILDER_LIB:gerac-gh/gerac.jar" \
-    $(find compiler/src -name "*.java") \
-    -d compiler/out/compiler
-cd "compiler/out/compiler"
-jar -cf ../compiler.jar *
-cd ../../..
+# Compile WebCompiler if not present
+if [ ! -d "build-cache/webc-out" ]; then
+    mkdir -p "build-cache/webc-out"
+    javac \
+        -cp "build-cache/gerac.jar:build-cache/bytecoder-api.jar" \
+        $(find "compiler/src" -name "*.java") \
+        -d "build-cache/webc-out"
+fi
 
-java -cp "compiler/out/builder.jar:$BUILDER_LIB" typesafeschwalbe.Builder \
-    gerac-gh/gerac.jar compiler/out/compiler.jar \
-    typesafeschwalbe.gerac.web.WebCompiler \
-    build/playground/gerac-impl.js
+# Compile gerac to Javascript if not present
+rm -rf "build-cache/bytecoder-in"
+mkdir "build-cache/bytecoder-in"
+cp -r "build-cache/webc-out/." "build-cache/bytecoder-in"
+cd "build-cache/bytecoder-in"
+jar -xvf "../gerac.jar"
+java -jar "../bytecoder.jar" compile js \
+    -classpath=. \
+    -mainclass=typesafeschwalbe.gerac.web.WebCompiler \
+    -builddirectory=..
+cd ../..
 
-rm -rf gerac-gh
+exit 0
 
 rm -rf std-gh
 git clone https://github.com/geralang/std std-gh
