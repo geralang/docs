60f281120a5938e345428ea729ac698f11b84886
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-05-28 19:29:05 +0300

    Small adjustments

--- build.sh
+++ build.sh
@@ -2,41 +2,54 @@
 
 rm -rf build
 
-cp -r ./src ./build
+# Prepare build directory
 
-cd ../test
+cp -r ./src ./build
+mkdir -p build-temp
+
+# Generate stdlib documentation
+if [ ! -d "build-temp/gerap_doc_proj" ]; then
+    cd "build-temp"
+    gerap new "gerap_doc_proj"
+    cd ..
+fi
+cd "build-temp/gerap_doc_proj"
 gerap doc std
-cd ../docs
-cp -r ../test/.gerap/docs ./build/docs
+cd ../..
+cp -r "build-temp/gerap_doc_proj/.gerap/docs" "./build/docs"
+
+# Compile gerac to Javascript
 
 BUILDER_LIB=$(find 'compiler/libs' -type f | tr '\n' ':')
 
-#rm -rf gerac-gh
-#git clone https://github.com/geralang/gerac gerac-gh
-#cd gerac-gh
-#make
-#cd ..
-mkdir gerac-gh -p
-cp gerac.jar gerac-gh/gerac.jar
+# # if '../gerac/gerac.jar' is present:
+# mkdir gerac-gh -p
+# cp ../gerac/gerac.jar gerac-gh/gerac.jar
+# # else:
+rm -rf gerac-gh
+git clone https://github.com/geralang/gerac gerac-gh
+cd gerac-gh
+make
+cd ..
 
-mkdir compiler/out -p
+mkdir build-temp/out -p
 javac \
     -cp "$BUILDER_LIB" \
     $(find compiler/builder -name "*.java") \
-    -d compiler/out/builder
-cd "compiler/out/builder"
+    -d build-temp/out/builder
+cd "build-temp/out/builder"
 jar -cef typesafeschwalbe.Builder ../builder.jar *
 cd ../../..
 javac \
     -cp "$BUILDER_LIB:gerac-gh/gerac.jar" \
     $(find compiler/src -name "*.java") \
-    -d compiler/out/compiler
-cd "compiler/out/compiler"
+    -d build-temp/out/compiler
+cd "build-temp/out/compiler"
 jar -cf ../compiler.jar *
 cd ../../..
 
-java -cp "compiler/out/builder.jar:$BUILDER_LIB" typesafeschwalbe.Builder \
-    gerac-gh/gerac.jar compiler/out/compiler.jar \
+java -cp "build-temp/out/builder.jar:$BUILDER_LIB" typesafeschwalbe.Builder \
+    gerac-gh/gerac.jar build-temp/out/compiler.jar \
     typesafeschwalbe.gerac.web.WebCompiler \
     build/playground/gerac-impl.js
 
